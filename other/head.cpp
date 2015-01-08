0de0d4cb7f93b27b82b44f7af5827f96df44dff4
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-01-08 12:59:15 -0500

    tc to head

--- other/head.cpp
+++ other/head.cpp
@@ -1,34 +1,73 @@
+/* base IO */
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <string>
-#include <cstdlib>
 #include <cctype>
-#include <map>
-#include <set>
-#include <bitset>
-#include <queue>
-#include <deque>
-#include <list>
+#include <fstream>
+#include <sstream>
+
+/* data structure */
 #include <vector>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+/* alrotihm and math */
 #include <algorithm>
+#include <random>
 #include <cmath>
-#include <complex>
+
+/* run-time */
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+
+/* system dependent */
+#include <sys/time.h>
+
 using namespace std;
 
-#define len(x) (x).size()
+#define VI vector<int>
+#define VD vector<double>
+#define PII pair<int, int>
+#define PDD pair<double, double>
 #define PB push_back
-#define VI vector<int> 
-#define pii pair<int, int>
-typedef long long int64;
+#define MP make_pair
+#define len(x) ((x).size())
 
-const int inf = ~0u >> 1;
+const int inf = INT_MAX;
+const long long inf_ll = 0x7f7f7f7f;
 const double eps = 1e-8;
-const double pi = acos(-1.0);
-int sgn(double x)
-{
-	return fabs(x) < eps ? 0:x < -eps?-1:1;
-}
-int main()
-{
+
+/*
+ * 0 1 2
+ * 3   4
+ * 5 6 7
+ */
+const int bfs_dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int bfs_dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+/*
+ *   3  
+ * 2   0
+ *   1  
+ */
+const int dfs_dy[] = {0, 1, 0, -1};
+const int dfs_dx[] = {1, 0, -1, 0};
+
+PII dir[] = {make_pair(0,1),make_pair(1,0),make_pair(0,-1),make_pair(-1,0)};
+/* TC HEADER END */
+
+class A {
+	public:
+		;
+};
+
+int main() {
+	A a;
 }
